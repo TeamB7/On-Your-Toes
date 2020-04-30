f8dd9f8fc2ac2032d6b86c9339030fb0bb061d73
sxio0003 <sxio0003@student.monash.edu>
2020-04-30 21:52:02 +1000

    url change

--- app/src/main/java/com/example/a5120app/SplashScreen.java
+++ app/src/main/java/com/example/a5120app/SplashScreen.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.example.a5120app;
 
 import android.content.Intent;
@@ -17,23 +17,4 @@ public class SplashScreen extends AppCompatActivity {
         finish();
     }
 }
-=======
-package com.example.a5120app;
 
-import android.content.Intent;
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class SplashScreen extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Intent intent = new Intent(  this,StartActivity.class);
-        startActivity(intent);
-        finish();
-    }
-}
->>>>>>> 6774dfa8ce789244192f56c4d32a1d9beb5c7b16
