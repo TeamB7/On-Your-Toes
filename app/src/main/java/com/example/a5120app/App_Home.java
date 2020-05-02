4d590e0089206752d2fcec356df3c616532c0dd9
Dylan <dgon0003@student.monash.edu>
2020-05-02 10:41:31 +1000

    Add Main screen Ui and enter name page

---
+++ app/src/main/java/com/example/a5120app/App_Home.java
@@ -0,0 +1,14 @@
+package com.example.a5120app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class App_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app__home);
+    }
+}
