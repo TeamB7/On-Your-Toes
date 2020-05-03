cb0c6fdafca6ce8b5fc267cdaee7cc8a2259c9db
Dylan <dgon0003@student.monash.edu>
2020-05-03 14:23:29 +1000

    Add Multiple Ui Screens and related image resources

--- app/src/main/java/com/example/a5120app/StartActivity.java
+++ app/src/main/java/com/example/a5120app/StartActivity.java
@@ -3,9 +3,9 @@ package com.example.a5120app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +15,8 @@ public class StartActivity extends AppCompatActivity {
             this.getSupportActionBar().hide();
         }
         catch (NullPointerException e){}
-    }
-}
+
+
+
+            }
+        }
